8e7035e1b8555e2b31bffbf937465a289003ecfc
Vaishnavi-Jagtap-07 <141642560+Vaishnavi-Jagtap-07@users.noreply.github.com>
2023-08-14 19:23:53 +0530

    Create swaping.py
    Program for swapping without using third variable.

---
+++ swaping.py
@@ -0,0 +1,12 @@
+
+print("        Swapping without using 3rd variable        ")
+print("\n")
+a=int(input("Enter 1st value : "))
+b=int(input("Enter 2nd value : "))
+a=a+b
+b=a-b
+a=a-b
+print("\n")
+print("After swaping : ",a)
+print("After swaping : ",b)
+ 
